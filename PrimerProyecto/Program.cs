19522cbf949a1ef87a7406b6639892fcf2a33ead
Edgar  Guerrero <edgarguerrero37@gmail.com>
2023-03-29 22:46:27 -0500

    Add project files.

---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Programa que calcula el área de un rectángulo
+int ladoA; //Declaración de variables
+int ladoB;
+int resultado;
+
+Console.WriteLine("Calcula el área de un rectángulo!");
+Console.WriteLine("Ingrese el valor del lado A: ");
+
+ladoA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor del lado B: ");
+
+ladoB = Convert.ToInt32(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El resultado es " + resultado);
+ 
